0d07fac661d87dbfc2e642b7d35f821c345c5841
Alon Sapozhnikov <bitonallen11@gmail.com>
2022-06-21 11:27:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+
+namespace Dotnet_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                Console.WriteLine("  {0} = {1}", de.Key, de.Value);
+
+            Process[] prl = Process.GetProcesses();
+            foreach (Process tp in prl)
+                Console.WriteLine("Process: {0} ID: {1}", tp.ProcessName, tp.Id);
+        }
+    }
+}
