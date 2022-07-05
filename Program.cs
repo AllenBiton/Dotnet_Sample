1f371f2853f31da5dac54160c64acc68d59efce0
Alon Sapozhnikov <bitonallen11@gmail.com>
2022-07-05 08:34:51 +0300

    modified Program.cs to be simple

--- Program.cs
+++ Program.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Collections;
-using System.Diagnostics;
-using System.Threading;
 
 namespace Dotnet_Sample
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
 
-            //foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
-            //    Console.WriteLine("  {0} = {1}", de.Key, de.Value);
-
-            Process[] prl = Process.GetProcesses();
-            foreach (Process tp in prl)
-                if (tp.ProcessName.Contains("dotnet"))
-                    Console.WriteLine("Process: {0} ID: {1}", tp.ProcessName, tp.Id);
-
-            Thread.Sleep(100000);
+            return 99;
         }
     }
 }
